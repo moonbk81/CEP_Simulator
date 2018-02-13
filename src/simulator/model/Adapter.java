3ffb6f12adc95a6c631086b56b3a3f2b08bf0035
bongki.moon <bongki.moon@samsung.com>
2018-02-13 17:56:52 +0900

    add adapter

--- src/simulator/model/Adapter.java
+++ src/simulator/model/Adapter.java
@@ -1,6 +1,29 @@
 package simulator.model;
 
 public abstract class Adapter {
+    protected int complexity;
+    protected int consumption;
+
     public Adapter() {}
 
+    public int getComplexity() {
+        return complexity;
+    }
+
+    public void setComplexity(int complexity) {
+        this.complexity = complexity;
+    }
+
+    public int getConsumption() {
+        return consumption;
+    }
+
+    public void setConsumption(int consumption) {
+        this.consumption = consumption;
+    }
+
+    public void setAdapterData() {
+        // Random
+
+    }
 }
